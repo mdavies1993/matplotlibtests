86477b5496339e6180138b2db98ef6cdbab016b5
Mike Davies <bsmd@leeds.ac.uk>
2017-08-24 14:41:47 +0100

    biotin_conc_tht_plot.py updated to change location of legend. \n setupTk.py updated to add savefig button and callback

--- biotin_conc_tht_plot.py
+++ biotin_conc_tht_plot.py
@@ -1,37 +1,47 @@
 #!/usr/bin/python3
+from setupTk import Setup
 import numpy as np 
 import matplotlib as mpl
-from setupTk import Setup
 import pandas as pd
 import modules as mods
-fig_master = Setup()
-ax = fig_master.ax()
 
-raw_data = pd.read_csv('biotin_conc_tht.csv',skiprows=6,header=None).values[:,2:]
-x = raw_data[0,1:]/(60**2)
-y = raw_data[1:,:]
-y,keys = mods.group_data(y)
-labels = np.loadtxt('biotin_conc_tht_labels.txt',dtype='str')
-for label in labels:
-    print(label)
-icol = 0
-scatterplts = []
-smplsize = np.linspace(0,1,len(keys)-3)
-colors=[]
-for col in smplsize:
-    colors.append((1-col,0,0))
-for col in range(len(keys)-len(smplsize)):
-    colors.append('C'+str(int(col)))
-for group in y:
-    scplt = ax.plot(np.transpose(x),group,marker='o',markeredgecolor=colors[icol],ls='', ms=2.5,fillstyle='none',label=labels[icol])
-    for i in range(len(scplt)):
-        if i != 2:
-            scplt[i].set_label('_none_')
-    scatterplts.append(scplt)
-    icol +=1
-ax.set_xlim(np.amin(x),np.amax(x))
-legend = ax.legend()
-ax.set_xlabel(r'$Time\ (hrs)$')
-ax.set_ylabel(r'Flourescence 560\textit{nm} (au)')
-fig_master.draw()
-fig_master.master.mainloop()
+dataInPath = './biotin_conc_tht.csv'
+
+def onSave(figureToSave):
+    figureToSave.savefig(dataInPath[:-3]+'png',dpi=600)
+def main():
+    fig_master = Setup(onSave)
+    ax = fig_master.ax()
+
+    raw_data = pd.read_csv(dataInPath,skiprows=6,header=None).values[:,2:]
+    x = raw_data[0,1:]/(60**2)
+    y = raw_data[1:,:]
+    y,keys = mods.group_data(y)
+    labels = np.loadtxt('biotin_conc_tht_labels.txt',dtype='str')
+    for label in labels:
+        print(label)
+    icol = 0
+    scatterplts = []
+    smplsize = np.linspace(0,1,len(keys)-3)
+    colors=[]
+    for col in smplsize:
+        colors.append((1-col,0,0))
+    for col in range(len(keys)-len(smplsize)):
+        colors.append('C'+str(int(col)))
+    for group in y:
+        scplt = ax.plot(np.transpose(x),group,marker='o',markeredgecolor=colors[icol],ls='', ms=2.5,fillstyle='none',label=labels[icol])
+        for i in range(len(scplt)):
+            if i != 2:
+                scplt[i].set_label('_none_')
+        scatterplts.append(scplt)
+        icol +=1
+    ax.set_xlim(np.amin(x),np.amax(x))
+    legend = ax.legend()
+    legend._loc = 1
+    legend.set_bbox_to_anchor((1,0.6))
+    ax.set_xlabel(r'$Time\ (hrs)$')
+    ax.set_ylabel(r'Flourescence 560\textit{nm} (au)')
+    fig_master.draw()
+    fig_master.master.mainloop()
+
+if __name__=='__main__':main()
--- setupTk.py
+++ setupTk.py
@@ -9,16 +9,28 @@ import scipy as sp
 import pandas as pd
 
 class Setup():
-    def __init__(self):
+    def __init__(self, saveCall=None):
+        self.saveCall = saveCall
         self.master = Tk()
         self.fig = mpl.figure.Figure(figsize=(6,5),dpi=100)
         self.canvas = fctk(self.fig,master = self.master)
         self.fig_manager = fmtk(self.canvas,0,self.master)
         self.fig_manager.show()
         self.master.protocol("WM_DELETE_WINDOW", self.on_closing)
+        self.saverButton = Button(self.master, text="Save Fig", command=self.saveFigCall)
+        self.saverButton.pack()
         #self.master.after(200,self.draw)
         #self.canvas.show()
         #self.canvas.get_tk_widget().pack(side=TOP,fill=BOTH,expand=1)
+    def saveFigCall(self):
+        if self.saveCall is not None:
+            self.saveCall(self.fig)
+            return
+        save_dpi = 600
+        fig_no = 1
+        save_path = './figure'+str(fig_no)+'.png'
+        print('figure saved at %s dpi'%(str(save_dpi)))
+         
     def ax(self):
         return self.fig.add_subplot(111)
     def draw(self):
